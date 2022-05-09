354a128622e180cc8b9f168524332ccd2792cc10
farhioussama1969 <farhioussama1969@gmail.com>
2022-05-09 03:26:59 +0100

    update states

--- app/Http/Controllers/States/StatesController.php
+++ app/Http/Controllers/States/StatesController.php
@@ -10,7 +10,7 @@ class StatesController extends Controller
 {
     public function statesList(Request $request){
         $lang = $request->header('lang');
-        $statesResponse = DB::table('states')->select('id', "name_{$lang} AS name")->orderBy('id')->paginate(48);
+        $statesResponse = DB::table('states')->select('id', "name_{$lang} AS name", 'latitude', 'longitude')->orderBy('id')->paginate(48);
         return $statesResponse;
     }
 }
