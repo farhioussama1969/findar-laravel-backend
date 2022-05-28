45e34b8492233c648265e49b00467e6df2efe20d
farhioussama1969 <farhioussama1969@gmail.com>
2022-05-28 22:51:30 +0100

    add state

--- app/Http/Controllers/States/StatesController.php
+++ app/Http/Controllers/States/StatesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Image;
 
 class StatesController extends Controller
 {
