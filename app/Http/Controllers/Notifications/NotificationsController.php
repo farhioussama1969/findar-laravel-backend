c2067adc09d1b015536f969f18b9caf395e86540
farhioussama1969 <farhioussama1969@gmail.com>
2022-06-04 01:31:37 +0100

    notifications

--- app/Http/Controllers/Notifications/NotificationsController.php
+++ app/Http/Controllers/Notifications/NotificationsController.php
@@ -5,6 +5,7 @@ use Kreait\Firebase\Factory;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Messaging\AndroidConfig;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
 
@@ -16,11 +17,23 @@ class NotificationsController extends Controller
         $factory = (new Factory)->withServiceAccount(__DIR__.'/firebase-config.json');
         $messaging = $factory->createMessaging();
 
+        $config = AndroidConfig::fromArray([
+            'ttl' => '3600s',
+            'priority' => 'height',
+            'notification' => [
+                'title' => '$GOOG up 1.43% on the day',
+                'body' => '$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
+                'icon' => 'stock_ticker_update',
+                'color' => '#f45342',
+                'sound' => 'default',
+            ],
+        ]);
+
 
         $message = CloudMessage::withTarget('token', $fcmToken)
             ->withData($body);
 
-        $messaging->send($message);
+        $messaging->send($config);
     }
 
 //    public function testingNotification(Request $request){
