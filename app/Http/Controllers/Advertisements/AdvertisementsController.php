df7037b6659d2af4e5fdfbcfafe94e99a3eb16b4
farhioussama1969 <farhioussama1969@gmail.com>
2022-06-08 23:29:37 +0100

    update advertisement

--- app/Http/Controllers/Advertisements/AdvertisementsController.php
+++ app/Http/Controllers/Advertisements/AdvertisementsController.php
@@ -455,7 +455,6 @@ class AdvertisementsController extends Controller
 
     public function updateAdvertisement(Request $request){
 
-        return $request;
         $user = request()->user();
 
         $request->validate([
